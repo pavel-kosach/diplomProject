66cd68b14d7bcbac554f2b36876f7a6b8ff1e076
Pavel Kosach <kosachdev@gmail.com>
2023-03-18 20:07:20 +0300

    add scrollToTop btn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import modal from "./modules/modal";
+import slider from "./modules/slider";
+import sendForm from "./modules/sendForm";
+import upArrow from "./modules/upArrow";
 
 modal();
+slider();
+// sendForm();
+upArrow();
