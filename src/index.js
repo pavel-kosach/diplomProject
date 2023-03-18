e275d875b707354612dbe5a860c46ea39df6fa31
Pavel Kosach <kosachdev@gmail.com>
2023-03-18 21:11:09 +0300

    add top slider

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import modal from "./modules/modal";
 import slider from "./modules/slider";
 import sendForm from "./modules/sendForm";
 import upArrow from "./modules/upArrow";
+import faq from "./modules/faq";
 
 modal();
 slider();
 // sendForm();
 upArrow();
+faq();
